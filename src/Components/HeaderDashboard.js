8fb73196798d50d386655482484680b5ea094fca
AndreaValeroSanz <avalerosanz@uoc.edu>
2024-10-11 15:33:35 +0200

    commit en development porque soy una malota hehe

---
+++ src/Components/HeaderDashboard.js
@@ -0,0 +1,58 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <div>
+          <div class="row pt-5">
+            <div class="col-lg-2">
+              <div class="d-flex justify-content-center">
+                <h1>Dashboard</h1>
+              </div>
+            </div>
+            <div class="col-lg-10 d-flex justify-content-end">
+              invite
+            </div>
+          </div>
+  
+          <div>
+            <div class="">
+              <ul class="list-unstyled d-flex justify-content-end">
+                <li class="py-1 px-3">
+                  <button type="button" class="btn boton">
+                    <i class="bi bi-circle-fill pe-2" style="color: #4da167;"></i>
+                    MobileApp
+                  </button>
+                </li>
+  
+                <li class="py-1 px-3">
+                  <button type="button" class="btn boton">
+                    <i class="bi bi-circle-fill pe-2" style="color: #2589bd;"></i>
+                    MobileApp
+                  </button>
+                </li>
+  
+                <li class="py-1 px-3">
+                  <button type="button" class="btn boton">
+                    <i class="bi bi-circle-fill pe-2" style="color: #f5bb00;"></i>
+                    MobileApp
+                  </button>
+                </li>
+  
+                <li class="py-1 px-3">
+                  <button type="button" class="btn boton">
+                    <i class="bi bi-circle-fill pe-2" style="color: #ed98b4;"></i>
+                    MobileApp
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>`;
+    }
+  }
+  
+  customElements.define("my-header", Header);
+  
