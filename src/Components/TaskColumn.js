81e90c3d5053961b2fd2d9774f00adc0ed654821
FerranBCalpe <fcalper@uoc.edu>
2024-10-14 11:49:39 +0200

    actualización_columnas
    añadido iconos, Actuliazdo el botón de añadir tarea, aunque no tien el formato que quiero (está colocado a saco con el link de svg)
    ahora me estoy peleando con la sombra que no me sale.
    Voy a probar de usar un z-index menor para hacer una copia y ponerla detrás

--- src/Components/TaskColumn.js
+++ src/Components/TaskColumn.js
@@ -8,21 +8,33 @@ class TaskColumn extends HTMLElement {
   // Se llama cuando el elemento se conecta al DOM
   connectedCallback() {
     const title = this.getAttribute("title"); // Obtener el atributo 'title'
-    this.render(title); // Llamar al método render y pasar el título
+    const taskIcon = this.getAttribute("taskIcon"); // Obtener el atributo 'taskIcon'
+    this.render(title, taskIcon); // Llamar al método render y pasar el título e icono
   }
 
   // Método render para construir el HTML del componente
-  render(title) {
+  render(title, taskIcon) {
     this.shadowRoot.innerHTML = `
-        <style>
+      <style>
         .task-column {
-            background-color: #f8f8f8;
+            background-color: #F8F8F8;
             border-radius: 8px;
             padding: 10px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 1);
             width: 200px;
             min-height: 300px;
+            height:90vh;
+            -webkit-box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+            -moz-box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+            border: 1px solid #dbdbdb;
         }
+            .task-icon-new-task {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-bottom: 10px;
+        }
+
         .task-column h2 {
             font-size: 1.2rem;
             font-weight: 500;
@@ -30,21 +42,52 @@ class TaskColumn extends HTMLElement {
             font-optical-sizing: auto;
             font-style: normal;
         }
+
         .task-column hr {
             border: 0;
             border-top: 2px solid #dbdbdb;
             margin: 10px 0;
         }
+
+        .task-column svg {
+            width: 40px;
+            height: 40px;
+            margin-bottom: 10px;
+        }
+
         .task-item {
             /*TODO: los postits*/
         }
-        </style>
-        <div class="task-column">
-            <h2>${title}</h2>
-            <hr>
-            <div class="task-item"></div>
+       
+      </style>
+
+
+      <div class="task-column">
+        
+
+        <div class="task-icon-new-task"> 
+          <div class="icon-container">
+          ${
+            taskIcon
+              ? `<img src="${taskIcon}" alt="task-icon" class="icon"/>`
+              : ""
+          }
+           </div>
+            <button type="button" class="btn-new-task">
+           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" style="color: #5030e5" viewBox="0 0 32 32">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+        </svg>
+        </button>
         </div>
-      `;
+
+        <h2>${title}</h2>
+        <hr>
+      </div>
+      <div>
+        <div class="task-item"></div>
+      </div>
+    `;
   }
 }
 
