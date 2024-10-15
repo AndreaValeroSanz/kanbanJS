842a37e3c643e9d0a991dbbe0222f40b4aab2661
Elena <elopezestr@uoc.edu>
2024-10-15 19:36:39 +0200

    colaboradores

---
+++ src/Components/Collaborators.js
@@ -0,0 +1,78 @@
+
+class Collaborators extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
+  
+    connectedCallback() {
+      this.shadowRoot.innerHTML = `
+        <style>
+        @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
+          .row {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+          }
+          .btn-group {
+            display: flex;
+          }
+          .collaborator-section {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          .btn-group .btn {
+            margin-left: -10px; 
+            border-radius: 50%; /* Hacer que los botones se vean redondos */
+            padding: 5px;
+            width: 40px;
+            height: 40px;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .btn-group .btn i {
+            font-size: 40px;
+          }
+          h1 {
+            margin: 0;
+          }
+          .invite-text {
+            margin-bottom: 5px;
+            font-size: 14px;
+          }
+        </style>
+  
+        <div>
+          <div class="row pt-5">
+            <div class="col-lg-10 d-flex justify-content-end align-items-center collaborator-section">
+           
+              <div class="btn-group" role="group" aria-label="Collaborators icons">
+                <button type="button" class="btn btn-outline-primary">
+                  <i class="bi bi-person-circle"></i>
+                </button>
+                <button type="button" class="btn btn-outline-primary">
+                  <i class="bi bi-person-circle"></i>
+                </button>
+                <button type="button" class="btn btn-outline-primary">
+                  <i class="bi bi-person-circle"></i>
+                </button>
+                <button type="button" class="btn btn-outline-primary">
+                  <i class="bi bi-person-circle"></i>
+                </button>
+                <button type="button" class="btn btn-outline-primary">
+                  <i class="bi bi-person-circle"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    }
+    
+  }
+
+  customElements.define("my-collaborators", Collaborators);
+  
+  
