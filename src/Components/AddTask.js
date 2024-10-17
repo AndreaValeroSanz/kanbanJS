58806caa6c97d8a657c5c0b23eddc3a04cfb1aac
Elena <elopezestr@uoc.edu>
2024-10-17 13:54:11 +0200

    "Añadir tarea" casi hecho

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -7,36 +7,87 @@ class AddTask extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal @mdo</button>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Open modal @fat</button>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal @getbootstrap</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >icono mas</button>
+  
+    
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            
+          <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Task Title</h1>
+           
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>           
+          </div>     
+          
+      
+          <div class="modal-body">
+            <p> Task Title*</p> 
+          <input type="text" class="form-control" id="message-text" required>
+            <form>
+              <div class="mb-3">
+                <label for="recipient-name" class="col-form-label">Task's Description:</label>
+                <textarea type="text" class="form-control" id="message-text" rows=7></textarea> 
+              </div>
+              <div class="mb-3">
+                <p> Deadline</p>
+                <input type="date" id="recipient-te">
+              </div>
+              <style> .row: after{content:""; display: table; clear: both;}</style>
+              <div class="row"> <style>.column{ float: left; width: 50%;}</style>
+                <div class="column" >   
+                  <p>Select workarea </p>  
+                      <div class="form-check">
+                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                      <label class="form-check-label" for="flexRadioDefault1">
+                        Front
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        Back
+                      </label>                  
+                    </div> 
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                      Server
+                      </label>                  
+                    </div> 
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                    Testing
+                    </label>                  
+                  </div>          
+                </div>
+                <div class="column" >      
+                  <p>Assigned Task Collaborators </p> 
+                  <i class="bi bi-person-circle"></i><button type="button" class="btn btn-primary btn-sm">add</button>
+                  
+                  <i class="bi bi-person-circle"></i><button type="button" class="btn btn-primary btn-sm">add</button>
+                  <i class="bi bi-person-circle"></i><button type="button" class="btn btn-primary btn-sm">add</button>
+             
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <div class="mb-3">
-                  <label for="recipient-name" class="col-form-label">Recipient:</label>
-                  <input type="text" class="form-control" id="recipient-name">
+                  <button type="button" class="btn btn-link">add more...</button>       
                 </div>
-                <div class="mb-3">
-                  <label for="message-text" class="col-form-label">Message:</label>
-                  <textarea class="form-control" id="message-text"></textarea>
-                </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Send message</button>
-            </div>
+              </div>
+            </form>
+
+          </div>
+          <div class="modal-footer">            
+            
+            <button type="button" class="btn btn-primary">Create task</button>
+
           </div>
         </div>
       </div>
+    </div>
+    
+      
+ 
+  </body>
     `;
   }
 }
